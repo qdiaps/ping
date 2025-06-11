6ed378b20e604215dd000c8ac16e3eef8a6bc6c3
qdiaps <qdiaps@gmail.com>
2025-06-11 13:43:33 +0300

    add docs

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Qdiaps\\Ping\\Ping",
+            "name": "Ping",
+            "summary": "",
+            "url": "classes/Qdiaps-Ping-Ping.html"
+        },                {
+            "fqsen": "\\Qdiaps\\Ping\\Ping\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "",
+            "url": "classes/Qdiaps-Ping-Ping.html#method___construct"
+        },                {
+            "fqsen": "\\Qdiaps\\Ping\\Ping\u003A\u003Acheck\u0028\u0029",
+            "name": "check",
+            "summary": "Run\u0020checking\u0020URL\u0020address",
+            "url": "classes/Qdiaps-Ping-Ping.html#method_check"
+        },                {
+            "fqsen": "\\Qdiaps\\Ping\\Ping\u003A\u003AgetUrl\u0028\u0029",
+            "name": "getUrl",
+            "summary": "",
+            "url": "classes/Qdiaps-Ping-Ping.html#method_getUrl"
+        },                {
+            "fqsen": "\\Qdiaps\\Ping\\Ping\u003A\u003A\u0024url",
+            "name": "url",
+            "summary": "",
+            "url": "classes/Qdiaps-Ping-Ping.html#property_url"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Qdiaps\\Ping",
+            "name": "Ping",
+            "summary": "",
+            "url": "namespaces/qdiaps-ping.html"
+        },                {
+            "fqsen": "\\Qdiaps",
+            "name": "Qdiaps",
+            "summary": "",
+            "url": "namespaces/qdiaps.html"
+        }            ]
+);
--- src/Ping.php
+++ src/Ping.php
@@ -4,18 +4,31 @@ namespace Qdiaps\Ping;
 
 class Ping
 {
+    /**
+     * @var string URL address
+     */
     protected string $url;
 
+    /**
+     * @param string $url
+     */
     public function __construct(string $url)
     {
         $this->url = $url;
     }
 
+    /**
+     * Run checking URL address
+     * @return bool Always TRUE
+     */
     public function check(): bool
     {
         return true;
     }
 
+    /**
+     * @return string URL address
+     */
     public function getUrl(): string
     {
         return $this->url;
